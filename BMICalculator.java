dbe128ec14c8acf7bd108441241eab2dac9d5a65
KitKoch <kamilnguyenwe@gmail.com>
2025-06-30 14:35:16 -0700

    BMI Calculator V2

--- BMICalculator.java
+++ BMICalculator.java
@@ -8,14 +8,16 @@ import java.util.*;
     30.0 <= BMI             Obese
 */
 
-public class BMICalculator {
+public final class BMICalculator {
     private static final double LB_TO_KG = 0.45359237;
     private static final double IN_TO_M = 0.0254;
     private static final double BMI_UNDER = 18.5;
     private static final double BMI_NORMAL = 25;
     private static final double BMI_OVER = 30;
 
-    private static double userPrompt(Scanner input, String msg) {
+    private BMICalculator(){}
+
+    private static double userDoublePositive(Scanner input, String msg) {
         while (true) {
             System.out.print(msg);
             if (input.hasNextDouble()) {
@@ -23,10 +25,11 @@ public class BMICalculator {
                 if (stat > 0) {
                     return stat;
                 }
+                System.out.println("Number cannot be negative");
             } else {
+                System.out.println("Please enter a number");
                 input.next();
             }
-            System.out.println("Please enter a positive number");
         }
     }
 
@@ -50,8 +53,8 @@ public class BMICalculator {
 
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
-            double weightInLB = userPrompt(input, "Please enter a number for weight: ");
-            double heightInIn = userPrompt(input, "Please enter a number for height: ");
+            double weightInLB = userDoublePositive(input, "Please enter a number for weight: ");
+            double heightInIn = userDoublePositive(input, "Please enter a number for height: ");
             double BMI = calculateBMI(weightInLB, heightInIn);
             System.out.printf("BMI is %.2f%n%s", BMI, bmiCategory(BMI));
         }
